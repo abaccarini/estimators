finished KSG implementation, corrected units from nats to bits

code/plot/plot_est_python.py
@@ -40,9 +40,18 @@ colors = [
 ]
 
 
-func_names = ["max", "min", "median", "median_min", "var", "var_nd", "var_mu", "var_nd_mu"]
+func_names = [
+    "max",
+    "min",
+    "median",
+    "median_min",
+    "var",
+    "var_nd",
+    "var_mu",
+    "var_nd_mu",
+]
 distributions = ["uniform_int", "uniform_real", "normal", "lognormal", "poisson"]
-data_dir = "../output/"
+data_dir = "../output_py/"
 fig_dir = "../figs_py/"
 
 
@@ -87,7 +96,15 @@ def getBounds(dist, param_str):
         raw_str = param_str[param_str.find("(") + 1 : param_str.find(")")]
         bounds = raw_str.split(",")
         return (int(bounds[0]), int(bounds[1]))
-
+    if dist == "lognormal":
+        raw_str = param_str[param_str.find("(") + 1 : param_str.find(")")]
+        bounds = raw_str.split(",")
+        return (0, 4 * float(bounds[1]))
+    if dist == "normal":
+        raw_str = param_str[param_str.find("(") + 1 : param_str.find(")")]
+        bounds = raw_str.split(",")
+        print(bounds)
+        return (-4 * float(bounds[1]), 4 * float(bounds[1]))
 
 
 def getParams(dist, param_str):
@@ -97,8 +114,7 @@ def getParams(dist, param_str):
     if dist == "uniform_int":
         raw_str = param_str[param_str.find("(") + 1 : param_str.find(")")]
         bounds = raw_str.split(",")
-        return (int(bounds[0]), int(bounds[1]) )
-
+        return (int(bounds[0]), int(bounds[1]))
 
 
 def plot_discrete(fname, dist, param_str):
@@ -114,28 +130,26 @@ def plot_discrete(fname, dist, param_str):
     fig, ax = plt.subplots()
     plt.ylabel(r"Entropy (bits)")
     plt.xlabel(r"Input  $x_A$")
-    
+
     plt.grid()
     plt.grid(which="minor", alpha=0.1)  # draw grid for minor ticks on x-axis
     ax2 = ax.twiny()
     ax2.spines["bottom"].set_position(("axes", -0.01))
     ax2.xaxis.set_ticks_position("top")
     ax2.spines["bottom"].set_visible(False)
-    
+
     if dist == "poisson":
         lam = getParams(dist, param_str)
-        ax2.axvline(lam, linestyle="--", alpha = 0.5, color='black')
+        ax2.axvline(lam, linestyle="--", alpha=0.5, color="black")
         ax2.set_xticks([lam])
-        ax2.set_xticklabels([r'$\lambda = %s$'%lam ], rotation=0, color='red')
+        ax2.set_xticklabels([r"$\lambda = %s$" % lam], rotation=0, color="red")
     if dist == "uniform_int":
-        a,b = getParams(dist, param_str)
-        print(a,b)
-        n = float( a + b-1 )/2.0
-        ax2.axvline(float( a + b -1)/2.0, linestyle="--", alpha = 0.5, color='black')
+        a, b = getParams(dist, param_str)
+        print(a, b)
+        n = float(a + b - 1) / 2.0
+        ax2.axvline(float(a + b - 1) / 2.0, linestyle="--", alpha=0.5, color="black")
         ax2.set_xticks([n])
-        ax2.set_xticklabels([r'$ \frac{a + b}{2}= %s$'%n ], rotation=0, color='red')
-
-    
+        ax2.set_xticklabels([r"$ \frac{a + b}{2}= %s$" % n], rotation=0, color="red")
 
     func_str = function_str(fname)
 
@@ -145,23 +159,22 @@ def plot_discrete(fname, dist, param_str):
     plot_lines = []
     cc = itertools.cycle(colors)
     alph = 0.5
-    
-    
-    oe_str = {0:'even', 1:'odd'}
+
+    oe_str = {0: "even", 1: "odd"}
     oe_key = 0
     t_init = json_data["target_init_entropy"]
     # print("bounds : ", lower_bound, upper_bound)
     print(json_data["awae_data"])
     # return
-    for numSpec,val in json_data["awae_data"].items():
+    for numSpec, val in json_data["awae_data"].items():
         if int(numSpec) < max_numspec:
             # if numSpec % 2 == oe_key:
 
             c = next(cc)
             # for jj, vv in val.items():
             #     print(jj, vv)
-            x_A = np.array([np.array(xi) for xi,vv in val.items()])
-            awae = np.array([t_init - np.array(vv) for xi,vv in val.items()])
+            x_A = np.array([np.array(xi) for xi, vv in val.items()])
+            awae = np.array([t_init - np.array(vv) for xi, vv in val.items()])
             # awae = np.array([np.array(xi) for xi in val])[:, 1]  # col slice, awaes
             label = r"$\lvert S\rvert\ = %s$" % numSpec
             (l2,) = plt.plot(
@@ -171,16 +184,13 @@ def plot_discrete(fname, dist, param_str):
                 color=c,
                 alpha=alph,
                 linestyle="-",
-                label=label
+                label=label,
             )
             plot_lines.append(l2)
 
-
-    legend1 = plt.legend(handles=plot_lines,
-                         loc="best",
-                         bbox_to_anchor=(1.32, 0.7),
-                         fontsize=14
-                         )
+    legend1 = plt.legend(
+        handles=plot_lines, loc="best", bbox_to_anchor=(1.32, 0.7), fontsize=14
+    )
     plt.gca().add_artist(legend1)
 
     plt.xticks(
@@ -212,7 +222,9 @@ def plot_discrete(fname, dist, param_str):
             label=target_init_label,
         )
     ]
-    legend2 = plt.legend(handles=hline_legened, loc="upper right", bbox_to_anchor=(1.3, 1.0), fontsize=14)
+    legend2 = plt.legend(
+        handles=hline_legened, loc="upper right", bbox_to_anchor=(1.3, 1.0), fontsize=14
+    )
     plt.gca().add_artist(legend2)
 
     plt.text(
@@ -224,7 +236,6 @@ def plot_discrete(fname, dist, param_str):
         va="bottom",
         bbox={"facecolor": "white", "alpha": 0.9, "pad": 3, "edgecolor": "black"},
     )
-    
 
     out_path = fig_dir + fname + "/" + dist
     Path(out_path).mkdir(parents=True, exist_ok=True)
@@ -236,13 +247,160 @@ def plot_discrete(fname, dist, param_str):
     plt.close("all")
 
 
+def plot_cont(fname, dist, param_str):
+    verify_args(fname, dist)
+    path = data_dir + fname + "/" + dist
+    files = os.listdir(path)
+    print(files)
+    file = [f for f in files if Path(f).stem == param_str][0]
+    print(file)
+    lower_bound, upper_bound = getBounds(dist, param_str)
+    print(lower_bound, upper_bound)
+    max_numspec = 6
+    fig, ax = plt.subplots()
+    plt.ylabel(r"Entropy (bits)")
+    plt.xlabel(r"Input  $x_A$")
+
+    plt.grid()
+    # plt.grid(which="minor", alpha=0.1)  # draw grid for minor ticks on x-axis
+    # ax2 = ax.twiny()
+    # ax2.spines["bottom"].set_position(("axes", -0.01))
+    # ax2.xaxis.set_ticks_position("top")
+    # ax2.spines["bottom"].set_visible(False)
+    # plt.xticks(np.arange(lower_bound,upper_bound, 1.0))
+
+    func_str = function_str(fname)
+
+    with open(path + "/" + file) as json_file:
+        json_data = json.load(json_file)
+
+    plot_lines = []
+    cc = itertools.cycle(colors)
+    alph = 0.5
+
+    t_init = json_data["target_init_entropy"]
+
+    for numSpec, val in json_data["awae_data"].items():
+        if int(numSpec) < max_numspec:
+            # if numSpec % 2 == oe_key:
+
+            c = next(cc)
+            # for jj, vv in val.items():
+            #     print(jj, vv)
+            x_A = np.array([float(xi) for xi, vv in val.items()])
+            print(x_A)
+            print(min(x_A))
+            print(max(x_A))
+            awae = np.array([t_init - np.array(vv) for xi, vv in val.items()])
+            # awae = np.array([np.array(xi) for xi in val])[:, 1]  # col slice, awaes
+            label = r"$\lvert S\rvert\ = %s$" % numSpec
+            (l2,) = plt.plot(
+                x_A, awae, marker="", color=c, alpha=alph, linestyle="-", label=label
+            )
+            plot_lines.append(l2)
+
+    legend1 = plt.legend(
+        handles=plot_lines, loc="best", bbox_to_anchor=(1.32, 0.7), fontsize=14
+    )
+    plt.gca().add_artist(legend1)
+
+    plt.xticks(
+        np.arange(lower_bound, upper_bound, 1), minor=True
+    )  # set minor ticks on x-axis
+    plt.yticks(
+        np.arange(lower_bound, upper_bound, 1), minor=True
+    )  # set minor ticks on y-axis
+    plt.tick_params(which="minor", length=0)  # remove minor tick lines
+
+    target_init_label = r"$H(X_T)$"
+    ax.hlines(
+        y=json_data["target_init_entropy"],
+        xmin=lower_bound,
+        xmax=(upper_bound),
+        linewidth=2,
+        color="black",
+        label=target_init_label,
+    )
+
+    hline_legened = [
+        Line2D(
+            [0],
+            [0],
+            color="black",
+            marker="",
+            alpha=1.0,
+            linestyle="-",
+            label=target_init_label,
+        )
+    ]
+    legend2 = plt.legend(
+        handles=hline_legened, loc="upper right", bbox_to_anchor=(1.3, 1.0), fontsize=14
+    )
+    plt.gca().add_artist(legend2)
+    plt.text(
+        0.8,
+        -0.2,
+        func_str,
+        transform=ax.transAxes,
+        ha="left",
+        va="bottom",
+        bbox={"facecolor": "white", "alpha": 0.9, "pad": 3, "edgecolor": "black"},
+    )
+
+    # plt.xticks(
+    #     np.arange(lower_bound, upper_bound, 1), minor=True
+    # )  # set minor ticks on x-axis
+    # plt.yticks(
+    #     np.arange(lower_bound, upper_bound, 1), minor=True
+    # )  # set minor ticks on y-axis
+    plt.tick_params(which="minor", length=0)  # remove minor tick lines
+
+    out_path = fig_dir + fname + "/" + dist
+    Path(out_path).mkdir(parents=True, exist_ok=True)
+    plt.savefig(
+        # out_path + "/" + param_str + "_" +oe_str[oe_key]+"_discrete_leakage.pdf",
+        out_path + "/" + param_str + "_cont_leakage.pdf",
+        bbox_inches="tight",
+    )
+    plt.close("all")
+
+
 def main():
+
+    # plot_cont("max", "normal", "(0.0,1.0)")
+    # plot_cont("max", "lognormal", "(0.0,1.0)")
+    # plot_cont("var", "normal", "(0.0,1.0)")
+    # plot_cont("var", "lognormal", "(0.0,1.0)")
+    # plot_cont("median", "normal", "(0.0,1.0)")
+    # plot_cont("median", "lognormal", "(0.0,1.0)")
+    # plot_cont("var_mu", "normal", "(0.0,1.0)")
+    # plot_cont("var_mu", "lognormal", "(0.0,1.0)")
+    # plot_cont("max", "normal", "(0.0,2.0)")
+    # plot_cont("max", "lognormal", "(0.0,2.0)")
+    # plot_cont("var", "normal", "(0.0,2.0)")
+    # plot_cont("var", "lognormal", "(0.0,2.0)")
+    # plot_cont("median", "normal", "(0.0,2.0)")
+    # plot_cont("median", "lognormal", "(0.0,2.0)")
+    # plot_cont("var_mu", "normal", "(0.0,2.0)")
+    # plot_cont("var_mu", "lognormal", "(0.0,2.0)")
+    # plot_cont("max", "normal", "(0.0,4.0)")
+    # plot_cont("max", "lognormal", "(0.0,4.0)")
+    # plot_cont("var", "normal", "(0.0,4.0)")
+    # plot_cont("var", "lognormal", "(0.0,4.0)")
+    # plot_cont("median", "normal", "(0.0,4.0)")
+    # plot_cont("median", "lognormal", "(0.0,4.0)")
+    # plot_cont("var_mu", "normal", "(0.0,4.0)")
+    # plot_cont("var_mu", "lognormal", "(0.0,4.0)")
+
+    # plot_discrete("var_mu", "uniform_int", "(0,3)")
+    # plot_discrete("var_nd_mu", "uniform_int", "(0,3)")
+    # plot_discrete("var_mu", "uniform_int", "(0,7)")
+
     plot_discrete("max", "uniform_int", "(0,4)")
     # plot_discrete("var", "uniform_int", "(0,7)")
     # plot_discrete("var_nd", "uniform_int", "(0,7)")
     # plot_discrete("median", "uniform_int", "(0,7)")
     # plot_discrete("median", "uniform_int", "(0,15)")
-
 
     # plot_discrete("var_mu", "uniform_int", "(0,3)")
     # plot_discrete("var_nd_mu", "uniform_int", "(0,3)")
@@ -252,7 +410,7 @@ def main():
     # # plot_discrete("max", "poisson", "(4)")
     # plot_discrete("var_nd", "poisson", "(4)")
     # plot_discrete("median", "poisson", "(4)")
-    
+
     # # plot_discrete("max", "poisson", "(8)")
     # plot_discrete("var_nd", "poisson", "(8)")
     # plot_discrete("median", "poisson", "(8)")

code/py_src/main.py
@@ -11,6 +11,13 @@ from dist_params import *
 
 np.random.seed(0)
 
+numIterations = 10
+maxNumSpecs = 11
+numT = 1
+numA = 1
+N = 1000
+k = 1
+
 
 class sampleData:
     def __init__(self, params, N, numT, numS, x_A, thefunc):
@@ -111,10 +118,11 @@ def calculateTargetInitEntropy(dist_params):
         if dist_params.lam > 10:
             return 0.5 * np.log2(2.0 * np.pi * np.e * dist_params.lam)
         else:
-            res = 0.0
+            accum = 0.0
             for i in range(0, 10 * dist_params.lam):
                 tmp = poisson.pmf(i, dist_params.lam)
                 accum += xlogy(tmp, tmp)
+            return (-1.0)*accum
 
     if isinstance(dist_params, lognormal_params):
         return (
@@ -126,12 +134,7 @@ def calculateTargetInitEntropy(dist_params):
 
 
 def batch_ex_uniform_int(fn: func):
-    numIterations = 100
-    maxNumSpecs = 11
-    numT = 1
-    numA = 1
-    N = 1000
-    k = 1
+
     # N_vals = np.array([4])
     N_vals = np.array([4, 8, 16])
     x_A_min = 0
@@ -165,14 +168,9 @@ def batch_ex_uniform_int(fn: func):
 
 
 def batch_ex_poisson(fn: func):
-    numIterations = 100
-    maxNumSpecs = 11
-    numT = 1
-    numA = 1
-    N = 1000
-    k = 1
+
     # N_vals = np.array([4])
-    lam_vals = np.array([4, 8, 16])
+    lam_vals = np.array([2, 4, 8])
     for lam in lam_vals:
         spec_to_xA_to_MI = {}
         params = poisson_params(lam)  # generates data from 0, 3-1
@@ -201,12 +199,7 @@ def batch_ex_poisson(fn: func):
 
 
 def batch_ex_normal(fn: func):
-    numIterations = 100
-    maxNumSpecs = 11
-    numT = 1
-    numA = 1
-    N = 1000
-    k = 1
+    
     # N_vals = np.array([4])
     mu = 0.0
     step_size = 0.1
@@ -219,7 +212,10 @@ def batch_ex_normal(fn: func):
         x_A_min = -4.0 * sigma
         x_A_max = 4.0 * sigma
 
-        x_A_range = np.linspace(x_A_min, x_A_max, num=(x_A_max - x_A_min) / step_size)
+        x_A_range = np.linspace(
+            # x_A_min, x_A_max, num=int((x_A_max - x_A_min) / step_size)
+            x_A_min, x_A_max, num=50
+        )
 
         for numSpecs in range(1, maxNumSpecs):
             xA_to_MI = {}
@@ -243,12 +239,7 @@ def batch_ex_normal(fn: func):
 
 
 def batch_ex_lognormal(fn: func):
-    numIterations = 100
-    maxNumSpecs = 11
-    numT = 1
-    numA = 1
-    N = 1000
-    k = 1
+
     # N_vals = np.array([4])
     mu = 0.0
     step_size = 0.1
@@ -261,7 +252,9 @@ def batch_ex_lognormal(fn: func):
         x_A_min = 0
         x_A_max = 4.0 * sigma
 
-        x_A_range = np.linspace(x_A_min, x_A_max, num=(x_A_max - x_A_min) / step_size)
+        x_A_range = np.linspace(
+            x_A_min, x_A_max, num=50
+        )
 
         for numSpecs in range(1, maxNumSpecs):
             xA_to_MI = {}
@@ -296,13 +289,30 @@ def main():
     # fn = func(np.sum, "sum")
     fn = func(np.max, "max")
     batch_ex_uniform_int(fn)
-    batch_ex_normal(fn)
-    batch_ex_lognormal(fn)
-    batch_ex_poisson(fn)
+    # batch_ex_lognormal(fn)
+    # batch_ex_normal(fn)
+    # batch_ex_poisson(fn)
 
-    # s = sampleData(params, 10, 1, 1, [1], np.sum)
+    fn = func(np.var, "var")
+    # batch_ex_uniform_int(fn)
+    # batch_ex_lognormal(fn)
+    # # batch_ex_normal(fn)
+    # batch_ex_poisson(fn)
 
-    # print("Mixed KSG: I(X:Y) = ", Mixed_KSG(s.x_T, s.O, k=5))
+    # fn = func(np.median, "median")
+    # batch_ex_uniform_int(fn)
+    # batch_ex_lognormal(fn)
+    # batch_ex_normal(fn)
+    # batch_ex_poisson(fn)
+
+    # def var_mu(x):
+    #     return np.asarray([np.var(x), np.mean(x)])
+
+    # fn = func(var_mu, "var_mu")
+    # batch_ex_uniform_int(fn)
+    # batch_ex_lognormal(fn)
+    # batch_ex_normal(fn)
+    # batch_ex_poisson(fn)
 
 
 if __name__ == "__main__":

code/py_src/mixed_ksg.py
@@ -1,6 +1,6 @@
 import scipy.spatial as ss
 from scipy.special import digamma
-from math import log
+from math import log, log2, e
 import numpy as np
 
 """
@@ -47,4 +47,4 @@ def Mixed_KSG(x, y, k=5):
             nx = len(tree_x.query_ball_point(x[i], knn_dis[i] - 1e-15, p=float("inf")))  # type: ignore
             ny = len(tree_y.query_ball_point(y[i], knn_dis[i] - 1e-15, p=float("inf")))  # type: ignore
         ans += (digamma(kp) + log(N) - digamma(nx) - digamma(ny)) / N
-    return ans
+    return ans * log2(e) # used to convert from nats to bits