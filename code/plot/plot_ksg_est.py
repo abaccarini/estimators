42fc08f9f31b7b147bb16c791016ddea03c1b941
Alessandro Baccarini <anbaccar@buffalo.edu>
2024-05-09 10:00:28 -0400

    more plotting updates

--- code/plot/plot_ksg_est.py
+++ code/plot/plot_ksg_est.py
@@ -1,6 +1,7 @@
 import json
 import numpy as np
 import os
+import math
 import sys
 import itertools
 import matplotlib.pyplot as plt
@@ -24,11 +25,11 @@ colors = [
     "green",
     "magenta",
     "darkorange",
-    "black",
-    "blue",
-    "darkgreen",
-    "magenta",
-    "darkorange",
+    "slategrey",
+    "cyan",
+    "blueviolet",
+    "dodgerblue",
+    "lightcoral",
 ]
 
 func_names = [
@@ -72,7 +73,8 @@ def function_str(fname):
     if fname == "var_nd_mu":
         return r"$f_{(\mu, \sigma^2)}(\vec{x}) =  8\cdot \mu + \sum_i (x_i - \mu)^2 $"
     if fname == "var_mu":
-        return r"$f_{(\mu, \sigma^2)}(\vec{x}) =   (\frac{1}{n}\sum_i x_i ,\frac{1}{n}\sum_i (x_i - \mu)^2)  $"
+        # return r"$f_{(\mu, \sigma^2)}(\vec{x}) =   (\frac{1}{n}\sum_i x_i ,\frac{1}{n}\sum_i (x_i - \mu)^2)  $"
+        return r"$f_{(\mu, \sigma^2)}(\vec{x}) =   (f_{\mu}(\vec{x}),f_{\sigma^2}(\vec{x}))  $"
     else:
         return ""
 
@@ -98,11 +100,11 @@ def getBounds(dist, param_str):
 def getParams(dist, param_str):
     if dist == "poisson":
         raw_str = param_str[param_str.find("(") + 1 : param_str.find(")")]
-        return int(raw_str)
+        return float(raw_str)
     if dist == "uniform_int":
         raw_str = param_str[param_str.find("(") + 1 : param_str.find(")")]
         bounds = raw_str.split(",")
-        return (int(bounds[0]), int(bounds[1]))
+        return (float(bounds[0]), float(bounds[1]))
     if dist == "lognormal":
         raw_str = param_str[param_str.find("(") + 1 : param_str.find(")")]
         bounds = raw_str.split(",")
@@ -140,17 +142,59 @@ def plot_discrete(fname, dist, param_str):
     ax2.spines["bottom"].set_visible(False)
 
     if dist == "poisson":
+        tick_upper = 3
         lam = getParams(dist, param_str)
-        ax2.axvline(lam, linestyle="--", alpha=0.5, color="black")
-        ax2.set_xticks([lam])
-        ax2.set_xticklabels([r"$\lambda = %s$" % lam], rotation=0, color="red")
+        mean = lam
+        stdev = np.sqrt(float(mean))
+        # ax2.axvline(lam, linestyle="--", alpha=0.5, color="black")
+        xtick = (
+            [mean + (-1.0) * stdev * i for i in range(1, tick_upper)]
+            + [mean]
+            + [mean + stdev * i for i in range(1, tick_upper)]
+        )
+        xlabels = (
+            [
+                r"$\lambda {-} %s \sigma$" % (i if i > 1 else "")
+                for i in range(1, tick_upper)
+            ]
+            + [r"$\lambda$"]
+            + [
+                r"$\lambda {+} %s \sigma$" % (i if i > 1 else "")
+                for i in range(1, tick_upper)
+            ]
+        )
+
+        ax2.set_xticks(xtick)
+        ax2.set_xticklabels(xlabels)
+        plt.xticks(fontsize=14)
+
+        # ax2.set_xticklabels([r"$\lambda = %s$" % lam], rotation=0, color="red")
     if dist == "uniform_int":
+        tick_upper = 2
         a, b = getParams(dist, param_str)
         # print(a, b)
-        n = float(a + b - 1) / 2.0
-        ax2.axvline(float(a + b - 1) / 2.0, linestyle="--", alpha=0.5, color="black")
-        ax2.set_xticks([n])
-        ax2.set_xticklabels([r"$ \frac{a + b}{2}= %s$" % n], rotation=0, color="red")
+        b = b - 1.0
+        mean = (a + b) / 2.0
+        stdev = np.sqrt(((b - a + 1.0) * (b - a + 1.0) - 1.0) / 12.0)
+        xtick = (
+            [mean - stdev * i for i in range(1, tick_upper)]
+            + [mean]
+            + [mean + stdev * i for i in range(1, tick_upper)]
+        )
+        xlabels = (
+            [r"$n {-} %s \sigma$" % (i if i > 1 else "") for i in range(1, tick_upper)]
+            + [r"$n$"]
+            + [
+                r"$n {+} %s \sigma$" % (i if i > 1 else "")
+                for i in range(1, tick_upper)
+            ]
+        )
+        # print("uniform a,b", a, b)
+        # print("uniform stdev", stdev)
+        # print("uniform xticks", xtick)
+        ax2.set_xticks(xtick)
+        ax2.set_xticklabels(xlabels)
+        plt.xticks(fontsize=14)
 
     func_str = function_str(fname)
 
@@ -167,10 +211,17 @@ def plot_discrete(fname, dist, param_str):
     # return
     if fname == "median" or fname == "median_min":
         max_numspec = 11
+    leakage_no_A = json_data["leakage_no_attacker"]
 
     for numSpec, val in json_data["awae_data"].items():
-        def plotfn():
-            c = next(cc)
+        leakage_val = leakage_no_A[numSpec]
+        # print("leakage_val", leakage_val)
+
+        def plotfn(col=None):
+            if col is None:
+                c = next(cc)
+            else:
+                c = col
             x_A = np.array([np.array(xi) for xi, vv in val.items()])
             awae = np.array([t_init - np.array(vv) for xi, vv in val.items()])
             label = r"$\lvert S\rvert\ = %s$" % numSpec
@@ -183,20 +234,30 @@ def plot_discrete(fname, dist, param_str):
                 linestyle="-",
                 label=label,
             )
+            ax.hlines(
+                y=t_init - leakage_val,
+                xmin=lower_bound,
+                xmax=(upper_bound) - 1,
+                linewidth=2,
+                color=c,
+                linestyle="--",
+            )
+
             plot_lines.append(l2)
 
         if int(numSpec) < max_numspec:
             if (int(numSpec) % 2 == oe_key) and (
                 fname == "median" or fname == "median_min"
             ):
-                plotfn()
+                col = colors[int(numSpec) - 1]
+                plotfn(col)
             elif fname != "median" and fname != "median_min":
                 plotfn()
 
-    legend1 = plt.legend(
-        handles=plot_lines, loc="best", bbox_to_anchor=(1.32, 0.7), fontsize=14
-    )
-    plt.gca().add_artist(legend1)
+    # legend1 = plt.legend(
+    #     handles=plot_lines, loc="best", bbox_to_anchor=(1.32, 0.7), fontsize=14
+    # )
+    # plt.gca().add_artist(legend1)
 
     plt.xticks(
         np.arange(lower_bound, upper_bound, 1), minor=True
@@ -222,15 +283,27 @@ def plot_discrete(fname, dist, param_str):
             [0],
             color="black",
             marker="",
-            alpha=1.0,
-            linestyle="-",
-            label=target_init_label,
+            alpha=0.9,
+            linestyle="--",
+            label=r"$H(X_T \mid O)$",
         )
     ]
     legend2 = plt.legend(
-        handles=hline_legened, loc="upper right", bbox_to_anchor=(1.3, 1.0), fontsize=14
+        handles=hline_legened, loc="best", 
+        # bbox_to_anchor=(1.3, 1.0),
+        fontsize=14
     )
     plt.gca().add_artist(legend2)
+
+    plt.text(
+        0.5,
+        0.95,
+        target_init_label,
+        transform=ax.transAxes,
+        ha="center",
+        va="center",
+        bbox={"facecolor": "white", "alpha": 0.95, "pad": 0, "edgecolor": "white"},
+    )
 
     plt.text(
         0.5,
@@ -271,6 +344,10 @@ def plot_cont(fname, dist, param_str):
     plt.xlabel(r"Input  $x_A$")
 
     plt.grid()
+    ax2 = ax.twiny()
+    ax2.spines["bottom"].set_position(("axes", -0.01))
+    ax2.xaxis.set_ticks_position("top")
+    ax2.spines["bottom"].set_visible(False)
 
     func_str = function_str(fname)
 
@@ -284,49 +361,112 @@ def plot_cont(fname, dist, param_str):
     if fname == "median" or fname == "median_min":
         max_numspec = 11
 
-    ax2 = ax.twiny()
     if dist == "normal":
         mu, sigma = getParams(dist, param_str)
-        ax2.axvline(mu, linestyle="--", alpha=0.5, color="black")
-        sig_line = sigma
-        ax2.axvline(sig_line, linestyle="--", alpha=0.5, color="black")
-        ax2.axvline((-1.0)*sig_line, linestyle="--", alpha=0.5, color="black")
-        ax2.set_xticks([mu])
-        ax2.set_xticklabels([r"mean $= %.2f$" % mu], rotation=0, color="black")
+        tick_upper = 4
+        xtick = (
+            [(-1.0) * sigma * i for i in range(1, tick_upper)]
+            + [mu]
+            + [sigma * i for i in range(1, tick_upper)]
+        )
+        xlabels = (
+            [
+                r"$\mu {-} %s \sigma$" % (i if i > 1 else "")
+                for i in range(1, tick_upper)
+            ]
+            + [r"$\mu$"]
+            + [
+                r"$\mu {+} %s \sigma$" % (i if i > 1 else "")
+                for i in range(1, tick_upper)
+            ]
+        )
+        ax2.set_xticks(xtick)
+        ax2.set_xticklabels(xlabels)
+        plt.xticks(fontsize=14, rotation=45)
+
     if dist == "lognormal":
+        # ax2 = ax.twiny()
         mu, sigma = getParams(dist, param_str)
-        mean = np.exp(mu + (sigma * sigma)/ 2)
-        ax2.axvline(mean, linestyle="--", alpha=0.5, color="black")
+        mean = np.exp(mu + (sigma * sigma) / 2)
+        # ax2.axvline(mean, linestyle="--", alpha=0.5, color="black")
         ax2.set_xticks([mean])
         ax2.set_xticklabels([r"mean $= %.2f$" % mean], rotation=0, color="black")
-        
-        
+    leakage_no_A = json_data["leakage_no_attacker"]
+
     for numSpec, val in json_data["awae_data"].items():
-        def plotfn(upper_bound):
-            c = next(cc)
+        leakage_val = leakage_no_A[numSpec]
+
+        def plotfn(upper_bound, col=None):
+            if col is None:
+                c = next(cc)
+            else:
+                c = col
             x_A = np.array([float(xi) for xi, vv in val.items()])
             if max(x_A) > upper_bound:
                 upper_bound = max(x_A)
             awae = np.array([t_init - np.array(vv) for xi, vv in val.items()])
+            # awae = [x for x in awae if x > lower_bound and x < upper_bound]
             yhat = savgol_filter(awae, 51, 3)  # window size 51, polynomial order 3
             label = r"$\lvert S\rvert\ = %s$" % numSpec
             (l2,) = plt.plot(
                 x_A, yhat, marker="", color=c, alpha=alph, linestyle="-", label=label
             )
             plot_lines.append(l2)
+            ax.hlines(
+                y=t_init - leakage_val,
+                xmin=lower_bound,
+                xmax=(upper_bound),
+                linewidth=2,
+                color=c,
+                linestyle="--",
+            )
+
+        # if int(numSpec) < max_numspec:
+        #     if (int(numSpec) % 2 == oe_key) and (
+        #         fname == "median" or fname == "median_min"
+        #     ):
+        #         plotfn(upper_bound)
+        #     elif fname != "median" and fname != "median_min":
+        #         plotfn(upper_bound)
 
         if int(numSpec) < max_numspec:
             if (int(numSpec) % 2 == oe_key) and (
                 fname == "median" or fname == "median_min"
             ):
-                plotfn(upper_bound)
+                col = colors[int(numSpec) - 1]
+                plotfn(upper_bound, col)
             elif fname != "median" and fname != "median_min":
                 plotfn(upper_bound)
 
     legend1 = plt.legend(
-        handles=plot_lines, loc="best", bbox_to_anchor=(1.32, 0.7), fontsize=14
+        handles=plot_lines,
+        loc="best",
+        bbox_to_anchor=(1.32, 0.7),
+        fontsize=14,
+        # handles=plot_lines,
+        # loc="best",
+        # fontsize=14,
     )
-    plt.gca().add_artist(legend1)
+    # plt.gca().add_artist(legend1)
+
+    hline_legened = [
+        Line2D(
+            [0],
+            [0],
+            color="black",
+            marker="",
+            alpha=0.9,
+            linestyle="--",
+            label=r"$H(X_T \mid O)$",
+        )
+    ]
+    legend2 = plt.legend(
+        handles=hline_legened, loc="best", 
+        # bbox_to_anchor=(1.3, 1.0),
+        fontsize=14
+    )
+    plt.gca().add_artist(legend2)
+
 
     plt.xticks(
         np.arange(lower_bound, upper_bound, 1), minor=True
@@ -343,24 +483,20 @@ def plot_cont(fname, dist, param_str):
         xmax=(upper_bound),
         linewidth=2,
         color="black",
-        label=target_init_label,
+        # label=target_init_label,
     )
 
-    hline_legened = [
-        Line2D(
-            [0],
-            [0],
-            color="black",
-            marker="",
-            alpha=1.0,
-            linestyle="-",
-            label=target_init_label,
-        )
-    ]
-    legend2 = plt.legend(
-        handles=hline_legened, loc="upper right", bbox_to_anchor=(1.3, 1.0), fontsize=14
+    plt.text(
+        0.5,
+        0.95,
+        target_init_label,
+        transform=ax.transAxes,
+        ha="center",
+        va="center",
+        bbox={"facecolor": "white", "alpha": 0.95, "pad": 0, "edgecolor": "white"},
     )
-    plt.gca().add_artist(legend2)
+
+    # plt.gca().add_artist(legend2)
 
     plt.text(
         0.5,
@@ -386,7 +522,52 @@ def plot_cont(fname, dist, param_str):
             out_path + "/" + param_str + "_cont_leakage.pdf",
             bbox_inches="tight",
         )
-    plt.close("all")
+        all_specs_legend = [
+            Line2D(
+                [0],
+                [0],
+                color=colors[int(numSpec) - 1],
+                marker="",
+                alpha=1.0,
+                linestyle="-",
+                label=r"$\lvert S\rvert\ = %s$" % numSpec,
+            )
+            for numSpec in range(1, 11)
+        ]
+
+        # fig, ax2 = plt.subplots()
+        # plt.gca().add_artist(legend1)
+        legend1 = plt.legend(
+            handles=all_specs_legend,
+            loc="center right",
+            bbox_to_anchor=(1.0, 0.3),
+            prop={"size": 16},
+        )
+        plt.autoscale(enable=True, axis="y", tight=True)
+        figl2, axl2 = plt.subplots(figsize=(0, 0))
+        axl2.axis(False)
+
+        def flip(items, ncol):
+            return itertools.chain(*[items[i::ncol] for i in range(ncol)])
+
+        reorder = lambda l, nc: sum((l[i::nc] for i in range(nc)), [])
+
+        axl2.legend(
+            handles=reorder(all_specs_legend, 5),
+            loc="center",
+            bbox_to_anchor=(0.5, 0.5),
+            columnspacing=0.5,
+            borderpad=0.5,
+            fontsize="small",
+            ncol=5,
+            handlelength=1,
+        )
+        figl2.savefig(
+            fig_dir + "legend_text_only" + ".pdf",
+            bbox_inches="tight",
+            pad_inches=0,
+        )
+        plt.close("all")
 
 
 def main():
@@ -402,7 +583,7 @@ def main():
 
     cont_dists = [
         "normal",
-        "lognormal",
+        # "lognormal",
     ]
 
     global oe_key
@@ -413,23 +594,23 @@ def main():
                 print(dname, p_str)
                 plot_cont(func, dname, p_str)
 
-    # for dname in disc_dists:
-    #     for func in func_names:
-    #         for p_str in param_strs_dict[dname]:
-    #             print(dname, p_str)
-    #             plot_discrete(func, dname, p_str)
+    for dname in disc_dists:
+        for func in func_names:
+            for p_str in param_strs_dict[dname]:
+                print(dname, p_str)
+                plot_discrete(func, dname, p_str)
 
-    # oe_key = 1
-    # for dname in cont_dists:
-    #     for func in func_names:
-    #         for p_str in param_strs_dict[dname]:
-    #             print(dname, p_str)
-    #             plot_cont(func, dname, p_str)
-    # for dname in disc_dists:
-    #     for func in func_names:
-    #         for p_str in param_strs_dict[dname]:
-    #             print(dname, p_str)
-    #             plot_discrete(func, dname, p_str)
+    oe_key = 1
+    for dname in cont_dists:
+        for func in func_names:
+            for p_str in param_strs_dict[dname]:
+                print(dname, p_str)
+                plot_cont(func, dname, p_str)
+    for dname in disc_dists:
+        for func in func_names:
+            for p_str in param_strs_dict[dname]:
+                print(dname, p_str)
+                plot_discrete(func, dname, p_str)
 
 
 if __name__ == "__main__":
